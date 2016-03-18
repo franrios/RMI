85315c6ae9c7c7770e38a3c8ee1f71474e13acbd
Fran Rios <fcojriosbello@gmail.com>
2016-03-18 01:47:40 +0100

    Methods implemented

--- servidor/ControllerImpl.java
+++ servidor/ControllerImpl.java
@@ -5,24 +5,41 @@ import java.util.*;
 
 class ControllerImpl extends UnicastRemoteObject implements Controller {
 
-    private List<Ap> aplist;
-    private List<Ssid> ssidlist;
-    private List<Station> stationlist;
+    private List<Ap> apList;
+    private List<Association> associationList;
+
+  //  private List<Ssid> ssidlist;
+//    private List<Station> stationlist;
 
     ControllerImpl() throws RemoteException {
-        aplist = new LinkedList<Ap>();
-        ssidlist = new LinkedList<Ssid>();
-        stationlist = new LinkedList<Station>();
+        apList = new LinkedList<Ap>();
+      //  ssidlist = new LinkedList<Ssid>();
+        associationList = new LinkedList<Association>();
     }
 
-    // ??????????????
-    public Ap registerStation(Station station) throws RemoteException {
-        Ap ap = new ApImpl(station);
-        l.add(ap);
-        return ap;
+
+    private Association isStationConnected(Station station) throws RemoteException{
+      Association aux = null;
+
+      for (i: this.associationList) {
+        if (i.getStation().getMac() == station.getMac())
+        aux = i;
+      }
+      return aux;
     }
 
-    public List<Ap> getStations() throws RemoteException {
-        return l;
+    public Ap connect(Station station) throws RemoteException{
+      Association aux = isStationConnected(station);
+      defaultAp = apList.get(1);
+      if (aux != null){
+        aux.setAp(defaultAp);
+        }
+      else
+        associationList.add(new Association(defaultAp, station));
+      }
+    }
+
+    public Boolean registerAp(Ap ap) throws RemoteException{
+      return this.apList.add(ap);
     }
 }
