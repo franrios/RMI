5a6e15e77df0984ab12f19297349c59c81ef3ab9
Fran Rios <fcojriosbello@gmail.com>
2016-03-18 01:47:23 +0100

    MEthods added

--- servidor/Controller.java
+++ servidor/Controller.java
@@ -3,6 +3,8 @@ import java.rmi.*;
 import java.util.*;
 
 interface Controller extends Remote {
-  AP registerStation(Station station) throws RemoteException;
-  List<AP> getStations() throws RemoteException;
+  Association isStationConnected(Station station) throws RemoteException;
+  Ap connect(Station station) throws RemoteException; /*  Check if a user is already associated and reassociate it or
+                                                          create a new association */
+  Boolean registerAp(Ap ap) throws RemoteException;
 }
