a72948d07adc4af78a5d7826390131210ee27bc2
Fran Rios <fcojriosbello@gmail.com>
2016-03-18 01:07:07 +0100

    Ap modified

---
+++ servidor/Ap.java
@@ -0,0 +1,10 @@
+
+import java.rmi.*;
+
+interface Ap extends Remote {
+	void addSSID(String ssid) throws RemoteException;
+	void removeSSID(String ssid) throws RemoteException;
+	int getMaxSSID() throws RemoteException;
+	int getID() throws RemoteException;
+
+}
