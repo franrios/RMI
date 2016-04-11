bf798be543ce6d85735b26237af5d97fdd21ad4b
franrios <fcojriosbello@gmail.com>
2016-04-11 11:00:20 +0200

    Comments modified

--- servidor/Controller.java
+++ servidor/Controller.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 interface Controller extends Remote {
   Association isStationConnected(Station station) throws RemoteException;
-  Ap connect(Station station) throws RemoteException; /*  Check if a user is already associated and reassociate it or
-                                                          create a new association */
+  /*  Check if a user is already associated and reassociate it or create a new association */
+  Ap connect(Station station) throws RemoteException;
   Boolean registerAp(Ap ap) throws RemoteException;
 }
